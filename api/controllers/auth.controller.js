f70147f389850d77fec35afd026f9d476da055e6
zemzemabdelkarim <bdlkrmzmzm@gmail.com>
2025-05-14 13:20:34 +0100

    middleware and error handling
    add a middleware to handle errors instead of hard-coding them in the controller

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/User.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const newUser = new User({username, email, password:bcryptjs.hashSync(password)});
   try {
@@ -10,6 +10,6 @@ export const signup = async (req, res) => {
       message: "supposedly seccess",
     });
   } catch (err) {
-    res.status(500).json(err.message);
+    next(err);
   }
 };
