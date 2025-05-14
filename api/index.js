0668dd42a42a0541525a2fb702dac1c7c82d0172
zemzemabdelkarim <bdlkrmzmzm@gmail.com>
2025-05-14 09:25:40 +0100

    connection to database
    extablishing a connection between the bachend (express) and the database (mongoDB) using mongoose driver

--- api/index.js
+++ api/index.js
@@ -1,8 +1,21 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGOURI)
+  .then(() => {
+    console.log("mongodb connected!");
+  })
+  .catch((err) => {
+    console.log(`an error occured during connection to database!!\n\t${err}`);
+  });
 
 const PORT = 8080;
 const app = express();
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(`server is listenning on port ${PORT}`);
 });
