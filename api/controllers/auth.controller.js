0ba6f4c5a0a4161078713076b1c02d33843fef64
zemzemabdelkarim <bdlkrmzmzm@gmail.com>
2025-05-15 12:06:47 +0100

    api signin
    create signin route and controller with cookie management and error handling with the newly created utils/error.js file and errorHandler() function related with the middleware added to the index.js (main server)

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,14 +1,48 @@
 import User from "../models/User.model.js";
 import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
+//import dotenv from 'dotenv';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const newUser = new User({username, email, password:bcryptjs.hashSync(password)});
+  const newUser = new User({
+    username,
+    email,
+    password: bcryptjs.hashSync(password),
+  });
   try {
     await newUser.save();
     res.status(201).json({
       message: "user created",
     });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User Not Found!"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const {password: hashedPassword, ...rest} = validUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+    
+    /*
+    const expiryDate = new Date(Date.now() + 3600000) //for 1 hour
+    
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
+    */
   } catch (err) {
     next(err);
   }
